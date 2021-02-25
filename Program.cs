efe9ca9162b2c6dff96acd249092fee91d778304
hsr1991 <hsr1991@hotmail.co.uk>
2021-02-25 22:27:47 +0000

    both csv and json file work and logging finished

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using NLog;
 using NLog.Config;
 using NLog.Targets;
+using Newtonsoft.Json;
 
 
 namespace SupportBank
@@ -19,28 +20,71 @@ namespace SupportBank
             config.AddTarget("File Logger", target);
             config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
             LogManager.Configuration = config;
-            Logger.Error("testing my logger");
-            Logger.Info("blah");
 
-            string path = @"support-bank-resources\Transactions2014.csv";
-            var readText = File.ReadAllLines(path).ToList().Skip(1);
+            Logger.Info("\n ----------------- \n New Run: ");
+
+            Console.Write("What type of file type are you using? Please type either 'CSV' or 'JSON'. \n");
+            string filetype = Console.ReadLine();
+            while (filetype != "CSV" && filetype !="JSON") 
+            {
+                Logger.Error("Invalid selection. '" + filetype + "' is an invalid file type. User prompted to retry.");
+                Console.WriteLine("Invalid selection. '" + filetype + "' is an invalid file type.");
+                filetype = Console.ReadLine();
+            }
             List<Transaction> tList = new List<Transaction>();
             List<string> nameList = new List<string>();
-            foreach (string line in readText)
+            if (filetype=="JSON")
             {
-                string[] data = line.Split(',');
-                nameList.Add(data[1]);
-                nameList.Add(data[2]);
-                var transaction = new Transaction
+                string input = @"support-bank-resources\Transactions2013.json";
+                var jsonString = File.ReadAllText(input);
+                tList = JsonConvert.DeserializeObject<List<Transaction>>(jsonString);
+                foreach(var trans in tList)
                 {
-                    TransactionDate = DateTime.Parse(data[0]),
-                    FromName = data[1],
-                    ToName = data[2],
-                    Narrative = data[3],
-                    Amount = double.Parse(data[4])
-                };
-                tList.Add(transaction);
+                    nameList.Add(trans.FromName);
+                    nameList.Add(trans.ToName);
+                }
+               
             }
+            
+            if (filetype=="CSV")
+            {
+                string path = @"support-bank-resources\DodgyTransactions2015.csv";
+                // string path = @"support-bank-resources\Transactions2014.csv";
+                var readText = File.ReadAllLines(path).ToList().Skip(1);
+                
+                int linecounter=1;
+                foreach (string line in readText)
+                {
+                    linecounter+=1;
+                    string[] data = line.Split(',');
+                    nameList.Add(data[1]);
+                    nameList.Add(data[2]);
+                    if (DateTime.TryParse(data[0], out DateTime variable)==false)
+                    {
+                        Logger.Error("\n Line"+ linecounter+": '" + line +"' - Transaction Date is not in a valid Date Time format.\n Skipped Line.");
+                        if (double.TryParse(data[4], out double numberZ)== false)
+                            {
+                                Logger.Error("\n Line"+ linecounter+": '"+ line + "'  - Amount is not in a valid format.\n Skipped Line.");
+                            }
+                        continue; //skips to the next iteration without finishing this iteration
+                    }
+                    if (double.TryParse(data[4], out double numberY)== false)
+                    {
+                        Logger.Error("\n Line"+ linecounter+": '"+ line + "'  - Amount is not in a valid format.\n Skipped Line.");
+                        continue;
+                    }
+                    var transaction = new Transaction
+                    {
+                        TransactionDate = DateTime.Parse(data[0]),
+                        FromName = data[1],
+                        ToName = data[2],
+                        Narrative = data[3],
+                        Amount = double.Parse(data[4])
+                    };
+                    tList.Add(transaction);
+                }
+            }
+            
 
             List<Accounts> accountsList = new List<Accounts>();
             List<string> uniqueNameList = nameList.Distinct().ToList();
@@ -76,15 +120,31 @@ namespace SupportBank
 
             Console.Write("What information would you like? \n");
             Console.Write("To print out the names of each person, along with the total amount they owe or are owed, type 'List All'. \n");
-            Console.Write("To print out just one user account, with all transactions associated with it, type 'List[Account]'. ");
+            Console.Write("To print out just one user account, with all transactions associated with it, type 'List Account'. ");
             string selection = Console.ReadLine();
-
-            if (selection == "List[Account]")
+            while (selection != "List Account" && selection != "List All") 
+            {
+                Logger.Error("Invalid selection. '" + selection + "' is an invalid selection. User prompted to retry.");
+                Console.WriteLine("Invalid selection. '" + selection + "' is an invalid selection.");
+                selection = Console.ReadLine();
+            }
+            if (selection == "List Account")
             {
                 Console.Write("Please type in the name of the account you want to display e.g. 'Dan W'. ");
                 string inputName = Console.ReadLine();
+
+                while (uniqueNameList.Contains(inputName)==false) //use while instead of if so you can put in a wrong name as
+                // many times as you want. if will only let you do it once.
+                {
+                    Logger.Error(inputName + " is not a valid account name. User prompted to retry.");
+                    Console.WriteLine(inputName + " is not a valid account name. Please try again.");
+                    inputName = Console.ReadLine();
+                }
+              
+                
                 foreach (var acc in accountsList)
                 {
+                    
                     if (acc.AccountName == inputName)
                     {
                         Console.WriteLine("Account Name: " + acc.AccountName);
--- Transaction.cs
+++ Transaction.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using NLog;
 using NLog.Config;
 using NLog.Targets;
+using Newtonsoft.Json;
 
 
 namespace SupportBank
@@ -12,10 +13,15 @@ namespace SupportBank
     // private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
     class Transaction
     {
+         [JsonProperty("Date")]
         public DateTime TransactionDate {get; set;}
+        [JsonProperty("FromAccount")]
         public string FromName {get; set;}
+        [JsonProperty("ToAccount")]
         public string ToName {get; set;}
+        [JsonProperty("Narrative")]
         public string Narrative {get; set;}
+        [JsonProperty("Amount")]
         public double Amount {get; set;}
     }
 }
